3863c40915329662c81cdda9fe08b39efcb4a700
Ciesii <135537269+Ciesii@users.noreply.github.com>
2024-10-14 17:41:11 +0200

    Feature - add change password. (#3)

--- app/room_reserve/urls.py
+++ app/room_reserve/urls.py
@@ -36,6 +36,8 @@ urlpatterns = [
         auth_views.password_reset_done,
         name="account_reset_password_done",
     ),
+    path("password/change/", auth_views.password_change, name="account_password_change"),
+
     re_path(
         r"^password/reset/key/(?P<uidb36>[0-9A-Za-z]+)-(?P<key>.+)/$",
         auth_views.password_reset_from_key,
