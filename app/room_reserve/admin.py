23795e502cddf2f47f586e6a84f27ac4d7a4deb6
Oskar Ciesak <oskar.ciesak3@gmail.com>
2024-10-18 22:10:07 +0200

    Feature - room models

--- app/room_reserve/admin.py
+++ app/room_reserve/admin.py
@@ -1,3 +1,9 @@
 from django.contrib import admin
+from room_reserve.models.calendar import Room
 
-# Register your models here.
+
+@admin.register(Room)
+class RoomAdmin(admin.ModelAdmin):
+
+    list_display = ("room_number", "building_name", "capacity", "room_supervisor")
+    search_fields = ("room_number", "building_name", "room_supervisor__username")
--- app/room_reserve/models/__init__.py
+++ app/room_reserve/models/__init__.py
@@ -1,1 +1,2 @@
 from .auth import *  # NOQA isort:skip
+from .calendar import *  # NOQA isort:skip
