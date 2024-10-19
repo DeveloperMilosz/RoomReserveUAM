e75cbbdd5edce354b8f2b4367ee2db3bbf12b828
Ciesii <135537269+Ciesii@users.noreply.github.com>
2024-10-19 23:03:18 +0200

    Feature - room models (#5)

---
+++ app/room_reserve/models/calendar.py
@@ -0,0 +1,26 @@
+from django.db import models
+from django.utils.translation import gettext_lazy as _
+
+
+class Room(models.Model):
+
+    room_number = models.CharField(_("room number"), max_length=30)
+    building_id = models.IntegerField(_("building id"))
+    building_name = models.CharField(_("building name"), max_length=50)
+    capacity = models.IntegerField(_("capacity"), null=True, blank=True)
+    room_supervisor = models.ForeignKey(
+        "room_reserve.User",
+        on_delete=models.SET_NULL,
+        verbose_name=_("room supervisor"),
+        blank=True,
+        null=True,
+    )
+    # equipment = models.ManyToManyField(
+    #     "AdditionalEquipment", verbose_name=_("equipment"), blank=True, null=True
+    # )
+    created_at = models.DateTimeField(_("created at"), auto_now_add=True)
+    modified_at = models.DateTimeField(_("modified at"), auto_now=True)
+    is_updated = models.BooleanField(_("is updated"), default=True)
+
+    def __str__(self):
+        return f"Room {self.room_number}"
