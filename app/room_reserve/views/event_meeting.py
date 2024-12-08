315cb96b6857daa9f2e891b8f5fd851efd40fc69
Lao360 <milypawelec@gmail.com>
2024-12-08 22:38:42 +0000

    Create event with meetings (#32)
    * modified:   app/room_reserve/forms/event.py
    modified:   app/templates/pages/calendar/viewer.html
    
    * Event_with_meetings - creation

--- app/room_reserve/views/event_meeting.py
+++ app/room_reserve/views/event_meeting.py
@@ -1,59 +1,6 @@
 from django.shortcuts import render, redirect
 from django.contrib import messages
 from room_reserve.models import Event, Meeting, Lecturers, Room
-
-# def create_event_with_meetings(request):
-#     if request.method == "POST":
-#         event_name = request.POST.get("eventname")
-#         event_description = request.POST.get("eventdescription")
-#         start_date = request.POST.get("eventdatestart")
-#         end_date = request.POST.get("eventdateend")
-#         participants = request.POST.get("participants")
-
-#         event = Event.objects.create(
-#             name=event_name,
-#             description=event_description,
-#             start_date=start_date,
-#             end_date=end_date,
-#             event_type=Event.GENERAL_EVENT
-#         )
-
-#         segment_names = request.POST.getlist("segmentname[]")
-#         segment_lecturers = request.POST.getlist("segmentlecturer[]")
-#         segment_rooms = request.POST.getlist("segmentroom[]")
-#         segment_descriptions = request.POST.getlist("segmentdescription[]")
-#         segment_dates = request.POST.getlist("segmentdate[]")
-#         segment_start_times = request.POST.getlist("segmenttimestart[]")
-#         segment_end_times = request.POST.getlist("segmenttimeend[]")
-#         segment_participants = request.POST.getlist("segmentparticipants[]")
-
-#         for i in range(len(segment_names)):
-#             lecturer = Lecturers.objects.filter(id=segment_lecturers[i]).first()
-#             room = Room.objects.filter(id=segment_rooms[i]).first()
-
-#             meeting = Meeting.objects.create(
-#                 name_pl=segment_names[i],
-#                 description=segment_descriptions[i],
-#                 start_time=f"{segment_dates[i]} {segment_start_times[i]}",
-#                 end_time=f"{segment_dates[i]} {segment_end_times[i]}",
-#                 capacity=segment_participants[i],
-#                 event=event,
-#                 room=room
-#             )
-
-#             if lecturer:
-#                 meeting.lecturers.add(lecturer)
-
-#         messages.success(request, "Event and meetings created successfully!")
-#         return redirect("home")
-
-#     rooms = Room.objects.all()
-#     lecturers = Lecturers.objects.all()
-#     return render(request, "pages/calendar/create_event_with_meetings.html", {
-#         "rooms": rooms,
-#         "lecturers": lecturers,
-#     })
-
 import json
 from django.utils.dateparse import parse_datetime
 
