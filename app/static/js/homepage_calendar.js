725a1b3feac165d372cf30cabc40ab9b48afea31
DeveloperMilosz <miloszdzierzgowski@outlook.com>
2024-12-11 05:26:00 +0100

    Add calendar to meeting search

--- app/static/js/homepage_calendar.js
+++ app/static/js/homepage_calendar.js
@@ -227,10 +227,56 @@ const displayWeeklyMeetings = meetings => {
     });
 };
 
+
+function getMeetingFromTable() {
+    // Znajdź tabelę HTML na podstawie klasy
+    const table = document.querySelector('.tabela');
+    const rows = table.querySelectorAll('tbody tr');
+    // Tablica, która przechowa wyniki
+    const result = [];
+
+    // Iteruj przez każdy wiersz tabeli
+    rows.forEach(row => {
+        const cells = row.querySelectorAll('td');
+        
+        const title = cells[0].querySelector('.link').textContent.trim();
+
+        const startDate = cells[3].textContent.trim();
+        const startTime = cells[4].textContent.trim();
+        const endDate = cells[5].textContent.trim();
+        const endTime = cells[6].textContent.trim();
+        const id = parseInt(cells[8].textContent.trim(), 10);
+        const color = cells[9].textContent.trim();
+        cells[5].style.display="none";
+        cells[8].style.display="none";
+        cells[9].style.display="none";
+        const start_time = `${startDate}T${startTime}:00+00:00`;
+        const end_time = `${endDate}T${endTime}:00+00:00`;
+        
+        // Dodaj obiekt do wyników
+        result.push({
+            id: id,
+            title: title,
+            start_time: start_time,
+            end_time: end_time,
+            name_en: title, // Zakładam, że tytuł w języku angielskim jest taki sam
+            color: color // Domyślny kolor, jeśli nie jest dostarczony w tabeli
+        });
+    });
+    
+    return result;
+}
+
 async function fetchMeetings() {
-    try {
-        const response = await fetch('/get_meetings/');
-        const meetings = await response.json();
+        let meetings;
+        currentURL = window.location.href
+        if (currentURL === 'http://0.0.0.0:8000/') {
+            apiEndpoint = '/get_meetings/';
+            const response = await fetch(apiEndpoint);
+            meetings = await response.json();
+        }  else {
+            meetings = getMeetingFromTable()
+        }
 
         console.log(meetings)
 
@@ -249,9 +295,6 @@ async function fetchMeetings() {
 
             displayMeetings(weeklyMeetings);
         }
-    } catch (error) {
-        console.error('Error while getting events', error);
-    }
 }
 
 document.getElementById('toggleView').addEventListener('click', () => {
