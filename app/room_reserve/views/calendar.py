923a659077b9490a71c9f25219965698ca5bb7b6
DeveloperMilosz <miloszdzierzgowski@outlook.com>
2024-12-15 05:37:15 +0100

    get_meetings ordered by start_time, getting only necessary data form api

--- app/room_reserve/views/calendar.py
+++ app/room_reserve/views/calendar.py
@@ -34,24 +34,14 @@ def new_meeting(request):
 
 
 def get_meetings(request):
-    # Fetch only approved meetings
-    meetings = Meeting.objects.select_related("room", "event").prefetch_related("lecturers").filter(is_approved=True)
+    meetings = Meeting.objects.select_related("room", "event").prefetch_related("lecturers").filter(is_approved=True).order_by("start_time")
     events = [
         {
             "id": meeting.id,
-            "title": meeting.name_pl,
-            "meeting_type": meeting.meeting_type,
             "start_time": meeting.start_time.isoformat(),
             "end_time": meeting.end_time.isoformat(),
-            "name_en": meeting.name_en,
-            "room": meeting.room.room_number if meeting.room else None,
-            "description": meeting.description,
-            "lecturers": [f"{lecturer.first_name} {lecturer.last_name}" for lecturer in meeting.lecturers.all()],
-            "capacity": meeting.capacity,
+            "title": meeting.name_pl,
             "color": meeting.color,
-            "is_updated": meeting.is_updated,
-            "in_event": meeting.event is not None,
-            "event_name": meeting.event.name if meeting.event else None,
         }
         for meeting in meetings
     ]
