4c0aa60b45a8b0d498641d9748c3eee9ff98952d
ciesii <oskar.ciesak3@gmail.com>
2024-12-17 21:50:53 +0100

    Feature - My_reservation

--- app/room_reserve/views/profile_views.py
+++ app/room_reserve/views/profile_views.py
@@ -1,28 +1,41 @@
 from django.shortcuts import render
 from django.contrib.auth.decorators import login_required
-from room_reserve.models import Meeting
-
-
-@login_required
-def my_profile_view(request):
-    return render(request, "pages/profile/my_profile.html")
+from django.http import HttpResponseForbidden
+from room_reserve.models import Meeting, Event
 
 
 @login_required
 def my_reservations(request):
     user = request.user
 
-    # Filtruj rezerwacje użytkownika
-    pending_reservations = Meeting.objects.filter(submitted_by=user, is_approved=False, is_rejected=False)
-    approved_reservations = Meeting.objects.filter(submitted_by=user, is_approved=True)
-    rejected_reservations = Meeting.objects.filter(submitted_by=user, is_rejected=True)
+    # Ograniczenie widoku do ADMIN, LECTURER i GUEST
+    if user.user_type not in ["Admin", "Lecturer", "Guest"]:
+        return HttpResponseForbidden("Nie masz uprawnień do przeglądania tej strony.")
+
+    # Filtruj spotkania (Meeting) dla zalogowanego użytkownika
+    pending_meetings = Meeting.objects.filter(submitted_by=user, is_approved=False, is_rejected=False)
+    approved_meetings = Meeting.objects.filter(submitted_by=user, is_approved=True)
+    rejected_meetings = Meeting.objects.filter(submitted_by=user, is_rejected=True)
+
+    # Filtruj wydarzenia (Event) dla zalogowanego użytkownika
+    pending_events = Event.objects.filter(submitted_by=user, is_approved=False, is_rejected=False)
+    approved_events = Event.objects.filter(submitted_by=user, is_approved=True)
+    rejected_events = Event.objects.filter(submitted_by=user, is_rejected=True)
 
     return render(
         request,
         "pages/profile/my_reservations.html",
         {
-            "pending_reservations": pending_reservations,
-            "approved_reservations": approved_reservations,
-            "rejected_reservations": rejected_reservations,
+            "pending_meetings": pending_meetings,
+            "approved_meetings": approved_meetings,
+            "rejected_meetings": rejected_meetings,
+            "pending_events": pending_events,
+            "approved_events": approved_events,
+            "rejected_events": rejected_events,
         },
     )
+
+
+@login_required
+def my_profile_view(request):
+    return render(request, "pages/profile/my_profile.html")
