13949fd0f5a362f62557f3cf8d72fade7a290231
ciesii <oskar.ciesak3@gmail.com>
2025-01-09 18:07:19 +0100

    add_manual_template

--- app/room_reserve/views/home.py
+++ app/room_reserve/views/home.py
@@ -1,6 +1,11 @@
 from django.views.generic import TemplateView
 from django.contrib.auth.mixins import LoginRequiredMixin
+from django.shortcuts import render
 
 
 class HomeView(LoginRequiredMixin, TemplateView):
     template_name = "pages/home.html"
+
+
+def manual_view(request):
+    return render(request, "pages/manual.html")
